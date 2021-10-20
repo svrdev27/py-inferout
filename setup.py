dc3224c5b0ede794b5f047ae344e163ef5fb0a0e
svrdev27 <6680953+svrdev27@users.noreply.github.com>
2021-10-20 11:29:47 +0530

    requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from distutils.core import setup
 from setuptools import find_packages
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+#install_reqs = parse_requirements('requirements.txt', session='hack')
+#reqs = [str(ir.req) for ir in install_reqs]
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
   name = 'inferout',
@@ -15,11 +25,12 @@ setup(
   url = 'https://github.com/svrdev27/py-inferout',
   download_url = 'https://github.com/svrdev27/py-inferout/archive/v0.0.0-alpha.0.tar.gz',
   keywords = ['distributed', 'scale', 'ML', 'models', 'serving', 'inference', 'framework'],
-  install_requires=[
-          'aioredis>=2.0.0b1',
-          'aiohttp>=3.7.4',
-          'ConfigArgParse>=1.5.1'
-      ],
+  #install_requires=[
+  #        'aioredis>=2.0.0b1',
+  #        'aiohttp>=3.7.4',
+  #        'ConfigArgParse>=1.5.1'
+  #    ],
+  install_requires=required,
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
